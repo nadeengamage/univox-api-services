e9e9f90b35fc852d7a6dd09fa3e655df024f31ba
Nadeen Gamage <nadeengamage@gmail.com>
2020-03-03 22:58:37 +0530

    Created user model

---
+++ models/User.py
@@ -0,0 +1,26 @@
+"""
+    @author - Nadeen Gamage
+    @email - nadeengamage@gmail.com
+    @web - www.nadeengamage.com
+    @project - UnivoX
+
+    Description - Application users.
+"""
+
+from app import app, db
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    x_id = db.Column(db.String(50), unique=True)
+    username = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(80))
+    role = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
+    
+    def __init(self, x_id, username):
+        self.x_id = x_id
+        self.username = username
+        pass
