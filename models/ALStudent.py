32ef78bd9db13ac2d96e2bb599873bee16adb0b3
Nadeen Gamage <nadeengamage@gmail.com>
2020-03-09 20:07:14 +0530

    Added new columns to student tables

--- models/ALStudent.py
+++ models/ALStudent.py
@@ -14,12 +14,12 @@ class ALStudent(db.Model):
     __tablename__ = 'tbl_student_al_details'
 
     id = db.Column('id', db.Integer, primary_key=True)
-    student_id = db.Column('std_al_student_id', db.Integer, db.ForeignKey('tbl_students.id'), unique=True)
-    application_no = db.Column('std_application_no', db.String(100), unique=True)
+    student_id = db.Column('std_al_student_id', db.Integer, db.ForeignKey('tbl_students.id'), unique=True, nullable=False)
+    application_no = db.Column('std_application_no', db.String(100), unique=True, nullable=False)
     applicant = db.relationship('Student', backref='tbl_student_al_details', uselist=False)
-    stream = db.Column('std_al_stream', db.String(50), unique=True)
-    al_index_no = db.Column('std_al_index', db.String(50))
-    z_score = db.Column('std_al_z_score', db.String(20))
+    stream = db.Column('std_al_stream', db.String(50), unique=True, nullable=False)
+    al_index_no = db.Column('std_al_index', db.String(50), nullable=False)
+    z_score = db.Column('std_al_z_score', db.String(20), nullable=False)
     al_ict = db.Column('std_al_ict_grade', db.String(5))
     comm_and_media = db.Column('std_comm_and_media_grade', db.String(5))
     general_english = db.Column('std_al_gen_english_grade', db.String(5))
--- models/NVQStudent.py
+++ models/NVQStudent.py
@@ -14,8 +14,9 @@ class NVQStudent(db.Model):
     __tablename__ = 'tbl_student_nvq_details'
 
     id = db.Column('id',db.Integer, primary_key=True)
-    student_id = db.Column('std_nvq_student_id',db.Integer, db.ForeignKey('tbl_students.id'), unique=True)
-    application_no = db.Column('std_application_no', db.String(100), unique=True)
+    student_id = db.Column('std_nvq_student_id',db.Integer, db.ForeignKey('tbl_students.id'), unique=True, nullable=False)
+    application_no = db.Column('std_application_no', db.String(100), unique=True, nullable=False)
+    batch_type = db.Column('std_batch_type', db.String(4), unique=True)
     applicant = db.relationship('Student', backref='tbl_student_nvq_details', uselist=False)
     index_no = db.Column('std_nvq_index_no',db.String(50), unique=True)
     diploma = db.Column('std_nvq_diploma',db.String(80))
