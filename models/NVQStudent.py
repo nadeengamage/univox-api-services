9a4a4e32cae4d8bc845f078a20ea6a42dcaeace1
Nadeen Gamage <nadeengamage@gmail.com>
2020-03-10 08:25:25 +0530

    Update NVQStudent.py

--- models/NVQStudent.py
+++ models/NVQStudent.py
@@ -16,7 +16,7 @@ class NVQStudent(db.Model):
     id = db.Column('id',db.Integer, primary_key=True)
     student_id = db.Column('std_nvq_student_id',db.Integer, db.ForeignKey('tbl_students.id'), unique=True, nullable=False)
     application_no = db.Column('std_application_no', db.String(100), unique=True, nullable=False)
-    batch_type = db.Column('std_batch_type', db.String(4), unique=True)
+    batch_type = db.Column('std_batch_type', db.String(4))
     applicant = db.relationship('Student', backref='tbl_student_nvq_details', uselist=False)
     index_no = db.Column('std_nvq_index_no',db.String(50), unique=True)
     diploma = db.Column('std_nvq_diploma',db.String(80))
