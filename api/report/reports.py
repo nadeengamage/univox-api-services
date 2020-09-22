58a59bd9fedb967c4a7f8eff6077e24b69bbf4c1
Nadeen Gamage <nadeengamage@gmail.com>
2020-09-22 07:33:10 +0530

    Updated query

--- api/report/reports.py
+++ api/report/reports.py
@@ -3,7 +3,7 @@
     @email - warunanissanka44@gmail.com
     @project - UnivoX
 
-    Description - Report Controller.
+    Description - diploma Controller.
 """
 
 from app import app, db
@@ -15,6 +15,8 @@ from exceptions.validations import ValidationError
 from flask_jwt_extended import jwt_required, get_jwt_identity
 
 service = ReportService()
+
+BUFFER = 10
 
 # get NVQ/AL report data
 @app.route('/reports', methods=['POST'])
@@ -41,28 +43,34 @@ def get_report_data():
 
             if payload['stream'] == "NVQ":
                 # fetch generated data by the degree code
-                result = service.get_nvq_applicants_by_degree_code(criteria.btch_one_stud_per_program, degree.degree_code, "B1")
+                result = service.get_nvq_applicants_by_degree_code((criteria.btch_one_stud_per_program + BUFFER), degree.degree_code, "B1")
                 return jsonify({'message' : 'NVQ', 'result': [dict(row) for row in result]}), 200
 
             elif payload['stream'] == "AL":
                 # fetch generated data by the degree code
-                result = service.get_al_applicants_by_degree_code(criteria.btch_one_stud_per_program, degree.degree_code, "B1")
+                result = service.get_al_applicants_by_degree_code((criteria.btch_one_stud_per_program + BUFFER), degree.degree_code, "B1")
                 return jsonify({'message' : 'AL', 'result': [dict(row) for row in result]}), 200
 
             elif payload['stream'] == "ALL":
-                count = service.get_nvq_applicants_count_by_degree_code(criteria.btch_one_stud_per_program, degree.degree_code, "B1")
-                result = service.get_nvq_al_applicants_by_degree_code(count, criteria.btch_one_stud_per_program - count, degree.degree_code, "B1")
-                return jsonify({'message' : 'ALL', 'result': [dict(row) for row in result]}), 200
-
+                nvq_result = service.get_nvq_applicants_by_degree_code((criteria.btch_one_stud_per_program + BUFFER), degree.degree_code, "B1")
+                nvq_count = len([dict(row) for row in nvq_result])
+                
+                if (criteria.btch_one_stud_per_program + BUFFER) > nvq_count: 
+                    nvq_al_result = service.get_nvq_al_applicants_by_degree_code((nvq_count + BUFFER), ((criteria.btch_one_stud_per_program + BUFFER) - nvq_count), degree.degree_code, "B1")
+                    return jsonify({'message' : 'ALL_NVQ_AL', 'result': [dict(row) for row in nvq_al_result]}), 200
+                else:
+                    result = service.get_nvq_applicants_by_degree_code((criteria.btch_one_stud_per_program + BUFFER), degree.degree_code, "B1")
+                    return jsonify({'message' : 'ALL_NVQ', 'result': [dict(row) for row in result]}), 200
             else:
                 return jsonify({'error' : 'Invalid stream!'}), 400
 
         elif payload['batch_type'] == "B2":
             # generate the records
-            service.generate_nvq_report("B2", 'get_jwt_identity()')
+            service.generate_nvq_report("B2", get_jwt_identity())
 
             # fetch generated data by the degree code
-            applicants = service.get_applicants_b2_with_degree_code(criteria.btch_two_stud_per_program)
+            result = service.get_applicants_b2_with_degree_code((criteria.btch_two_stud_per_program + BUFFER), degree.degree_code)
+            return jsonify({'message' : 'NVQ', 'result': [dict(row) for row in result]}), 200
 
         else:
             return jsonify({'error' : 'Invalid Batch Type!'}), 400
